e7c77b008bcf7999fbc0b818e8ae15c0947e7ed6
Ryan Purpura <ryan@rpurp.com>
2020-02-04 02:03:14 -0800

    Added archive

--- ryme/src/index.jsx
+++ ryme/src/index.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import Month from './components/month';
 import MastHead from './components/masthead';
 import SinglePost from './components/single_post';
+import SiteFooter from './components/site_footer';
+import Archives from './components/archive';
 import _ from './style.css';
 
 import {
@@ -24,7 +26,10 @@ const Index = () => {
           <Route exact path="/" component={Month} />
           <Route exact path="/:year/:month" component={Month} />
           <Route exact path="/:year/:month/:day/:title" component={SinglePost} />
+          <Route exact path="/archives" component={Archives}/>
         </Switch>
+
+        <SiteFooter/>
       </Router>
     </>
   )
